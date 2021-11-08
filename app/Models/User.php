dff7e141513466a5bd06ef8a7c976a3687d28c8a
vblanco20 <valeriaablanco@yahoo.com>
2021-11-08 12:40:19 -0300

    Agregado de UserContacts

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $with = [
+        'userContacts',
+    ];
+
+    //Get contacts for user
+    public function userContacts()
+    {
+        return $this->hasMany('App\Models\UserContact')->orderBy('created_at');
+    }
 }
